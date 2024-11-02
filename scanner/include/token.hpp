aa116251437733268e1355aa402dded1e2a5c528
Mahmoud Abdelraouf <mahmoud126125@gmail.com>
2024-11-02 22:08:39 +0200

    add the token class

--- scanner/include/token.hpp
+++ scanner/include/token.hpp
@@ -1,1 +1,47 @@
 // Token class definition
+#ifndef TOKEN_HPP
+#define TOKEN_HPP
+
+#include <string>
+
+enum class TokenType
+{
+    IF,
+    THEN,
+    ELSE,
+    END,
+    REPEAT,
+    UNTIL,
+    READ,
+    WRITE,
+    ASSIGN,
+    LESSTHAN,
+    EQUAL,
+    PLUS,
+    MINUS,
+    MULT,
+    DIV,
+    OPENBRACKET,
+    CLOSEDBRACKET,
+    SEMICOLON,
+    IDENTIFIER,
+    NUMBER,
+    UNKNOWN
+};
+
+class Token
+{
+public:
+    Token(TokenType type, const std::string &value)
+        : type(type), value(value) {}
+
+    TokenType getType() const { return type; }
+    std::string getValue() const { return value; }
+    std::string toString() const;
+
+private:
+    TokenType type;
+    std::string value;
+};
+
+#endif // TOKEN_HPP
--- scanner/src/token.cpp
+++ scanner/src/token.cpp
@@ -1,1 +1,51 @@
 // Token class implementation
+#include "token.hpp"
+
+std::string Token::toString() const
+{
+    switch (type)
+    {
+    case TokenType::IF:
+        return "IF";
+    case TokenType::THEN:
+        return "THEN";
+    case TokenType::ELSE:
+        return "ELSE";
+    case TokenType::END:
+        return "END";
+    case TokenType::REPEAT:
+        return "REPEAT";
+    case TokenType::UNTIL:
+        return "UNTIL";
+    case TokenType::READ:
+        return "READ";
+    case TokenType::WRITE:
+        return "WRITE";
+    case TokenType::ASSIGN:
+        return "ASSIGN";
+    case TokenType::LESSTHAN:
+        return "LESSTHAN";
+    case TokenType::EQUAL:
+        return "EQUAL";
+    case TokenType::PLUS:
+        return "PLUS";
+    case TokenType::MINUS:
+        return "MINUS";
+    case TokenType::MULT:
+        return "MULT";
+    case TokenType::DIV:
+        return "DIV";
+    case TokenType::OPENBRACKET:
+        return "OPENBRACKET";
+    case TokenType::CLOSEDBRACKET:
+        return "CLOSEDBRACKET";
+    case TokenType::SEMICOLON:
+        return "SEMICOLON";
+    case TokenType::IDENTIFIER:
+        return "IDENTIFIER";
+    case TokenType::NUMBER:
+        return "NUMBER";
+    default:
+        return "UNKNOWN";
+    }
+}

6fdae0af61221ac80efa2797eafc3f626d626f50
semo <eslammoh5@gmail.com>
2024-11-23 08:07:17 +0200

    fix: enhance file handling.
    - Improved validation and directory creation

--- scanner/include/file_handler.hpp
+++ scanner/include/file_handler.hpp
@@ -9,6 +9,7 @@
 #ifndef FILE_HANDLER_HPP
 #define FILE_HANDLER_HPP
 
+#include <filesystem>
 #include <fstream>
 #include <iostream>
 #include <sstream>
@@ -25,63 +26,60 @@
  * The `TINY` namespace organizes all classes, functions, and utilities
  * related to the lexical analysis of the TINY programming language.
  */
-namespace TINY
-{
+namespace TINY {
+/**
+ * @namespace SCANNER
+ * @brief Contains all components related to the lexical analysis (scanning) of TINY language.
+ */
+namespace SCANNER {
+
+/**
+ * @class FileHandler
+ * @brief Provides utility functions for file input and output.
+ *
+ * The `FileHandler` class offers static methods to perform file operations,
+ * including reading files, writing tokens to files, and writing content to files.
+ */
+class FileHandler {
+   public:
     /**
-     * @namespace SCANNER
-     * @brief Contains all components related to the lexical analysis (scanning) of TINY language.
+     * @brief Reads the entire content of a file into a string.
+     *
+     * This method opens the file at the specified path and reads its contents into a single string.
+     * If the file cannot be opened, it throws a runtime exception.
+     *
+     * @param filePath Path to the file to be read.
+     * @return A string containing the file's content.
+     * @throws std::runtime_error if the file cannot be opened.
      */
-    namespace SCANNER
-    {
+    static std::string readFile(const std::string &filePath);
 
-        /**
-         * @class FileHandler
-         * @brief Provides utility functions for file input and output.
-         *
-         * The `FileHandler` class offers static methods to perform file operations,
-         * including reading files, writing tokens to files, and writing content to files.
-         */
-        class FileHandler
-        {
-        public:
-            /**
-             * @brief Reads the entire content of a file into a string.
-             *
-             * This method opens the file at the specified path and reads its contents into a single string.
-             * If the file cannot be opened, it throws a runtime exception.
-             *
-             * @param filePath Path to the file to be read.
-             * @return A string containing the file's content.
-             * @throws std::runtime_error if the file cannot be opened.
-             */
-            static std::string readFile(const std::string &filePath);
+    /**
+     * @brief Writes a vector of tokens to a file, each token on a new line.
+     *
+     * This method takes a vector of `Token` objects and writes their string representation to the file.
+     * If the `includePosition` flag is true, the position of each token is also included in the output.
+     *
+     * @param filePath Path to the file to be written.
+     * @param tokens A vector of `Token` objects to write to the file.
+     * @param includePosition Whether to include token position in the output (default is false).
+     * @throws std::runtime_error if the file cannot be opened.
+     */
+    static void writeTokens(const std::string &filePath, const std::vector<Token> &tokens, bool includePosition = false);
 
-            /**
-             * @brief Writes a vector of tokens to a file, each token on a new line.
-             *
-             * This method takes a vector of `Token` objects and writes their string representation to the file.
-             * If the `includePosition` flag is true, the position of each token is also included in the output.
-             *
-             * @param filePath Path to the file to be written.
-             * @param tokens A vector of `Token` objects to write to the file.
-             * @param includePosition Whether to include token position in the output (default is false).
-             * @throws std::runtime_error if the file cannot be opened.
-             */
-            static void writeTokens(const std::string &filePath, const std::vector<Token> &tokens, bool includePosition = false);
+    /**
+     * @brief Writes a string to a file.
+     *
+     * This method writes the provided string to the file at the specified path.
+     * If the file cannot be opened, it throws a runtime exception.
+     *
+     * @param filePath Path to the file to be written.
+     * @param content A string to write to the file.
+     * @throws std::runtime_error if the file cannot be opened.
+     */
+    static void writeFile(const std::string &filePath, const std::string &content);
+};
+}  // namespace SCANNER
+}  // namespace TINY
 
-            /**
-             * @brief Writes a string to a file.
-             *
-             * This method writes the provided string to the file at the specified path.
-             * If the file cannot be opened, it throws a runtime exception.
-             *
-             * @param filePath Path to the file to be written.
-             * @param content A string to write to the file.
-             * @throws std::runtime_error if the file cannot be opened.
-             */
-            static void writeFile(const std::string &filePath, const std::string &content);
-        };
-    } // namespace SCANNER
-} // namespace TINY
-
-#endif // FILE_HANDLER_HPP
+#endif  // FILE_HANDLER_HPP
--- scanner/src/file_handler.cpp
+++ scanner/src/file_handler.cpp
@@ -12,59 +12,85 @@
 
 #include "file_handler.hpp"
 
-namespace TINY
-{
-    /**
-     * @namespace SCANNER
-     * @brief Contains all components related to the lexical analysis (scanning) of TINY language.
-     */
-    namespace SCANNER
-    {
+namespace TINY {
+/**
+ * @namespace SCANNER
+ * @brief Contains all components related to the lexical analysis (scanning) of TINY language.
+ */
+namespace SCANNER {
 
-        std::string FileHandler::readFile(const std::string &filePath)
-        {
-            // Open the file at the given path
-            std::ifstream file(filePath);
+std::string FileHandler::readFile(const std::string &filePath) {
+    // file path cannot be empty
+    if (filePath.empty()) {
+        throw std::invalid_argument("File path cannot be empty.");
+    }
 
-            // Check if the file was opened successfully
-            if (!file.is_open())
-            {
-                throw std::runtime_error("Failed to open file: " + filePath);
-            }
+    // file path must exist
+    if (!std::filesystem::exists(filePath)) {
+        throw std::invalid_argument("File does not exist: " + filePath);
+    }
 
-            // Read the entire file content into a string
-            return std::string(
-                (std::istreambuf_iterator<char>(file)),
-                std::istreambuf_iterator<char>());
-        }
+    // Open the file at the given path
+    std::ifstream file(filePath);
 
-        void FileHandler::writeTokens(const std::string &filePath, const std::vector<Token> &tokens,
-                                      bool includePosition)
-        {
-            // Create a string file content from the tokens
-            std::ostringstream contentStream;
-            for (const Token &token : tokens)
-            {
-                contentStream << token.toString(includePosition) << "\n";
-            }
+    // Check if the file was opened successfully
+    if (!file.is_open()) {
+        throw std::runtime_error("Failed to open file: " + filePath);
+    }
 
-            // Write the file content to the file at the given path
-            FileHandler::writeFile(filePath, contentStream.str());
-        }
+    // Read the entire file content into a string
+    return std::string(
+        (std::istreambuf_iterator<char>(file)),
+        std::istreambuf_iterator<char>());
+}
 
-        void FileHandler::writeFile(const std::string &filePath, const std::string &content)
-        {
-            // Open the file at the given path
-            std::ofstream file(filePath);
+void FileHandler::writeTokens(const std::string &filePath, const std::vector<Token> &tokens,
+                              bool includePosition) {
+    // Create a string file content from the tokens
+    std::ostringstream contentStream;
+    for (const Token &token : tokens) {
+        contentStream << token.toString(includePosition) << "\n";
+    }
 
-            // Check if the file was opened successfully
-            if (!file.is_open())
-            {
-                throw std::runtime_error("Failed to open file: " + filePath);
-            }
+    // Write the file content to the file at the given path
+    FileHandler::writeFile(filePath, contentStream.str());
+}
 
-            // Write the content to the file
-            file << content;
-        }
-    } // namespace SCANNER
-} // namespace TINY
+void FileHandler::writeFile(const std::string &filePath, const std::string &content) {
+    // if the file path is empty, throw an invalid argument exception
+    if (filePath.empty()) {
+        throw std::invalid_argument("File path cannot be empty.");
+    }
+
+    // if the path is a directory, throw an invalid argument exception
+    if (std::filesystem::is_directory(filePath)) {
+        throw std::invalid_argument("File path is a directory: " + filePath);
+    }
+
+    // Extract the parent directory from the file path
+    std::filesystem::path fileParentPath = std::filesystem::path(filePath).parent_path();
+
+    // if the path have unmade folders, create them
+    if (!fileParentPath.empty() && !std::filesystem::exists(fileParentPath)) {
+        std::filesystem::create_directories(fileParentPath);
+        // set color to orange
+        std::cout << "\033[1;33m";
+        // print the message with the full path from the root
+        std::cout << "Creating directory: " << std::filesystem::absolute(fileParentPath) << std::endl;
+        // reset color
+        std::cout << "\033[0m";
+    }
+
+    // Open the file at the given path
+    std::ofstream file(filePath);
+
+    // Check if the file was opened successfully
+    if (!file.is_open()) {
+        throw std::runtime_error("Failed to open file: " + filePath);
+    }
+
+    // Write the content to the file
+    file << content;
+}
+}  // namespace SCANNER
+}  // namespace TINY
